b902af1dcf931bc3c6ba98d3ff18ce1bad11cf11
pBouris <pbouris6@yahoo.com>
2018-03-06 10:49:13 -0800

    made changes to employee UI

--- app/src/main/java/cecs491/android/csulb/edu/cecs491project/EmployeeHomePageActivity.java
+++ app/src/main/java/cecs491/android/csulb/edu/cecs491project/EmployeeHomePageActivity.java
@@ -3,27 +3,50 @@ package cecs491.android.csulb.edu.cecs491project;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
  * Created by Michael on 3/3/2018.
+ * editied by Peter 3/4/2018 - ongoing...
  */
 
-public class EmployeeHomePageActivity extends Activity {
+public class EmployeeHomePageActivity extends AppCompatActivity {
 
-    private TextView test;
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                case R.id.navigation_schedule:
+                    mTextMessage.setText(R.string.title_schedule);
+                    return true;
+                case R.id.navigation_announcements:
+                    mTextMessage.setText(R.string.title_announcement);
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_home_page);
 
-        test = (TextView) findViewById(R.id.employeeTestTextView);
-
-        Intent i = getIntent();
-        Bundle b = i.getExtras();
-        String firstName = b.getString("firstName");
-        test.setText("Welcome to your home page, " + firstName + "!");
-
-
+        mTextMessage = (TextView) findViewById(R.id.employeeTestTextView);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
