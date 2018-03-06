15588c53a075e57e12d6ff44fd63f06782124bc2
CandyTamer <lori.bergman.cs@gmail.com>
2018-03-06 10:51:17 -0800

    date basework?

--- app/src/main/java/cecs491/android/csulb/edu/cecs491project/EmployeeScheduleActivity.java
+++ app/src/main/java/cecs491/android/csulb/edu/cecs491project/EmployeeScheduleActivity.java
@@ -1,18 +1,26 @@
 package cecs491.android.csulb.edu.cecs491project;
 
 import android.content.Intent;
+//import android.icu.text.SimpleDateFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class EmployeeScheduleActivity extends AppCompatActivity {
 
-    private Spinner dropDown;
+    //private Spinner dropDown;
     private TextView employeeNameTextView;
+    private TextView displayedWeekTextView;
     private TextView scheduleTextView;
+    private Button backButton;
+    private Button forwardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +28,11 @@ public class EmployeeScheduleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_employee_schedule);
 
         employeeNameTextView = (TextView) findViewById(R.id.employeeTextView);
-        dropDown = (Spinner) findViewById(R.id.spinnerSchedule);
+        displayedWeekTextView = (TextView) findViewById(R.id.displayedWeekTextView);
+        //dropDown = (Spinner) findViewById(R.id.spinnerSchedule);
         scheduleTextView = (TextView) findViewById(R.id.scheduleTextView);
+        backButton = (Button) findViewById(R.id.backButton);
+        forwardButton = (Button) findViewById(R.id.forwardButton);
 
         Intent i = getIntent();
         Bundle b = i.getExtras();
@@ -29,22 +40,41 @@ public class EmployeeScheduleActivity extends AppCompatActivity {
         String lastName = b.getString("lastName");
         employeeNameTextView.setText("Employee:" + firstName + "  " + lastName);
 
-        dropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+//        dropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
 
     }
 
-//    @Override
-//    protected void onStart() {
-//        String[] items =
-//    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Date current = Calendar.getInstance().getTime();
+        Date begin = current;
+        SimpleDateFormat dayOfWeek = new SimpleDateFormat("EEE", Locale.US);
+
+        while (!dayOfWeek.format(begin).equals("SUN")) {
+            //Date test = new DateTime(current).minusDays(1).toDate();
+            begin = changeDate(begin, -1);
+        }
+
+        Date end = changeDate(begin, 6);
+        SimpleDateFormat proper = new SimpleDateFormat("MM/DD/YYYY", Locale.US);
+        displayedWeekTextView.setText(proper.format(begin) + " - " + proper.format(end));
+    }
+
+    private Date changeDate(Date date, int days) {
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, days);
+        return cal.getTime();
+    }
 }
