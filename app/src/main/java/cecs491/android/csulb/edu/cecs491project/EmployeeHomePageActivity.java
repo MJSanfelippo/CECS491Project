c3a1c0445c96b2bdc876ca98cd3ac6ab196966c0
MJSanfelippo <mj.sanfelippo@gmail.com>
2018-03-07 21:31:54 -0800

    Typo fix

--- app/src/main/java/cecs491/android/csulb/edu/cecs491project/EmployeeHomePageActivity.java
+++ app/src/main/java/cecs491/android/csulb/edu/cecs491project/EmployeeHomePageActivity.java
@@ -42,6 +42,7 @@ public class EmployeeHomePageActivity extends AppCompatActivity {
     private DatabaseReference ref;
     private String possibleShiftId;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
