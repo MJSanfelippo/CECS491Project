6c955faa14960a6b5294095877763f4547f9771b
Phanna Chrin <phannachrin@Phannas-MacBook-Pro.local>
2018-04-22 19:06:30 -0700

    Edit shift user interface

---
+++ app/src/main/java/cecs491/android/csulb/edu/cecs491project/EditEmployeeShift.java
@@ -0,0 +1,34 @@
+package cecs491.android.csulb.edu.cecs491project;
+
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+/**
+ * Created by phannachrin on 4/22/18.
+ */
+
+public class EditEmployeeShift extends AppCompatActivity {
+
+
+    private TextView dateTextView;
+    private TextView dayOfWeekTextView;
+
+    private EditText assignedByTextView;
+    private EditText startTimeEditTextView;
+    private EditText endTimeEditTextView;
+
+    private TimePickerDialog timePickerDialog;
+
+    private void instantiateLayout(){
+
+    }
+
+}
